6844bc94435fd2f2ff352a83ec2f0e235266833c
jkj8982@gmail.com <jkj>
2020-03-07 22:23:33 +0900

    observer, publisher, subscriber

---
+++ src/main/java/com/reactive/observer/MyObserver.java
@@ -0,0 +1,38 @@
+package com.reactive.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyObserver {
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++) {
+                setChanged();
+                notifyObservers(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Observer ob = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println(arg);
+            }
+        };
+
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.execute(io);
+
+        System.out.println(Thread.currentThread().getName() + "  EXIT");
+        es.shutdown();
+
+    }
+}
---
+++ src/main/java/com/reactive/observer/PubSub.java
@@ -0,0 +1,80 @@
+package com.reactive.observer;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Flow.*;
+
+public class PubSub {
+    public static void main(String[] args) {
+        Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5, 6);
+        ExecutorService es = Executors.newSingleThreadExecutor();
+
+        Publisher p = new Publisher() {
+            @Override
+            public void subscribe(Subscriber subscriber) {
+                Iterator<Integer> it = itr.iterator();
+
+                subscriber.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        es.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
+                                }
+                            } catch (Exception e) {
+                                subscriber.onError(e);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> s = new Subscriber<Integer>() {
+            Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println("onSubscription");
+                this.subscription = subscription;
+                subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("onNext " + item);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError : " + throwable.getMessage());
+                es.shutdown();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+                es.shutdown();
+            }
+        };
+
+        p.subscribe(s);
+
+    }
+}
