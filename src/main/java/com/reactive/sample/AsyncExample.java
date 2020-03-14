2a864c84bc7e2c1ae329ce99677ed83bae86603e
jkj8982@gmail.com <jkj>
2020-03-14 12:17:47 +0900

    Thread poll 설정

--- src/main/java/com/reactive/sample/AsyncExample.java
+++ src/main/java/com/reactive/sample/AsyncExample.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -23,6 +25,19 @@ public class AsyncExample implements ApplicationRunner {
             Thread.sleep(1000);
             return new AsyncResult<>("hello");
         }
+    }
+
+    @Bean
+    ThreadPoolTaskExecutor tp() {
+        ThreadPoolTaskExecutor tp = new ThreadPoolTaskExecutor();
+        tp.setCorePoolSize(10);
+        tp.setMaxPoolSize(100);
+        tp.setQueueCapacity(200);
+        tp.setThreadNamePrefix("myThread");
+        tp.initialize();
+        //core pool 생성 -> queue 생성 -> max pool 생성
+
+        return tp;
     }
 
     @Autowired
